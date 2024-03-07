75c6f16e89ed7fd7846bf7fb2cf74255d1d91d7d
Brady Bromley <bradybromley@live.ca>
2024-03-07 12:21:20 -0800

    Updated ER diagram

--- sql/scripts/Availability.sql
+++ sql/scripts/Availability.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Availability (
     ID int NOT NULL AUTO_INCREMENT,
     physicianID int NOT NULL,
-    startTime TIME NOT NULL DEFAULT '00:00:00',
-    endTime TIME NOT NULL DEFAULT '23:59:59',
+    startTime time NOT NULL DEFAULT '00:00:00',
+    endTime time NOT NULL DEFAULT '23:59:59',
     PRIMARY KEY (ID),
     FOREIGN KEY (physicianID) REFERENCES Users(ID)
 ) ENGINE=InnoDB CHARACTER SET utf8;
