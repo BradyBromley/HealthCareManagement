decd9ae8d7c3af9a5fbaa9c212dbf6eb30ac9389
Brady Bromley <bradybromley@live.ca>
2025-01-23 13:51:15 -0800

    Fixing gitignore issues

--- laravel-app/storage/framework/views/d2f8a8d975a1c868e40cb219fc296253.php
+++
@@ -1,6 +0,0 @@
-        </main>
-        <footer>
-            <!-- Empty for now -->
-        </footer>
-    </body>
-</html><?php /**PATH /var/www/HealthCareManagement/laravel-app/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
